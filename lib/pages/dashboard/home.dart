b12d187d9afd86070f497ae32957545028631432
noobie10123 <57508390+noobie10123@users.noreply.github.com>
2020-05-02 18:43:36 +0530

    Water done

--- lib/pages/dashboard/home.dart
+++ lib/pages/dashboard/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 class Home extends StatefulWidget {
 
 
@@ -19,17 +20,56 @@ class _HomeState extends State<Home> {
  Widget getListView() {
   var listView =ListView(
     children: <Widget>[
-      Container(
+      Card(
+      child: Container(
        padding:EdgeInsets.all(25) ,
        child: ListTile(
-        leading: Icon(Icons.favorite ,size :20,color:Colors.red),
+        leading: Icon(Icons.favorite ,size :30,color:Colors.red),
         title: Text('Calories :'),
         subtitle: Text("Calories Consumed for the Day")
       ),
-       )
+       ),
+      ),
+      Card(
+        child:Column(
+          children: <Widget>[
+          const ListTile(
+           contentPadding: EdgeInsets.fromLTRB(35, 25, 15, 15),
+          leading: Icon(Icons.invert_colors,size: 30,color:Colors.blue),
+          title: Text('Water '),
+          trailing: Icon(Icons.add_circle,size:40,color:Colors.blue,),
+        
+        ),
+         Container(
+           padding: EdgeInsets.all(25),
+           child:LinearPercentIndicator(
+                width: 140.0,
+                lineHeight: 14.0,
+                percent: 0.5,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+                center: Text(
+                  "50.0%",
+                  style: new TextStyle(fontSize: 12.0),
+              ),
+)
+         ),
+
+            
+          ],)
+      )
+
+
+    
     ]
   );
 
       return listView;
       
 }
+
+
+
+
+
+
