540cc4dec5688b3a13490ac74ba9b371842c71ba
aakash <p.saiaakash518@gmail.com>
2020-05-02 18:41:18 +0530

    chat done

---
+++ lib/pages/chats/chatsPage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'chatTile.dart';
+
+class ChatsPage extends StatefulWidget {
+  
+  @override
+  ChatsPageState createState() => ChatsPageState();
+}
+
+class ChatsPageState  extends State<ChatsPage>{
+
+  FirebaseUser user;
+  @override
+  void initState() {
+    _init();
+    super.initState();
+  }
+  _init()async{
+    FirebaseUser usertemp = await FirebaseAuth.instance.currentUser();
+
+    setState(() {
+      this.user = usertemp;
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('chats').snapshots(),
+        builder: (context,snapshot) {
+        
+          if(snapshot.hasData){
+            List<ChatTile> chats = new List();
+            List<DocumentSnapshot> ds = snapshot.data.documents;
+            
+            
+            if(this.user!=null){
+              
+              for(int i=0;i<ds.length;i++){
+                if(ds[i].documentID.contains(this.user.uid)){
+                  String profileName ;
+                  String profileUid;
+                  if(this.user.uid!=ds[i].data['uid1']){
+                    profileName = ds[i].data['name1'];
+                    profileUid = ds[i].data['uid1'];
+                  }
+                  else{
+                    profileName = ds[i].data['name2'];
+                    profileUid = ds[i].data['uid2'];
+
+                  }
+
+                  chats.add(
+                    ChatTile(
+                      messageCount: 0,
+                      myUid: this.user.uid,
+                      profileName: profileName,
+                      profileUid: profileUid,
+                      roomId: ds[i].documentID,
+                    )
+                  );
+                }
+              }
+            }
+            
+            return Scaffold(
+              appBar: AppBar(title: Text('Talk to Trainers'),),
+
+              body: ListView(
+                children: chats,
+              ),);
+          }
+          else return Scaffold(
+            appBar: AppBar(title: Text('Talk to Trainers'),),
+            body: Center(
+              child:CircularProgressIndicator() ,),
+          );
+        }
+      );
+  }
+}
