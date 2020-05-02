78bba379eeed5686969b400fe833141a789c12cb
aakash <p.saiaakash518@gmail.com>
2020-05-02 16:31:59 +0530

    signup sankalp

--- lib/pages/dashboard/home.dart
+++ lib/pages/dashboard/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../login.dart';
 class Home extends StatefulWidget {
 
 
@@ -7,9 +11,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GoogleSignIn googleSignIn = new GoogleSignIn(scopes: ['email']);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.exit_to_app),
+          onPressed: (){
+            FirebaseAuth.instance.signOut();
+            googleSignIn.signOut();
+            Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context)=>Login()));
+          },
+        )
+      ],title: Text('Home'),)
+      ,
             body: Center(child: Text('home')),
 
     );
