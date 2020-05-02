2fbabe2f08f721d80cacd5108a62ca1c7d9c28fe
noobie10123 <57508390+noobie10123@users.noreply.github.com>
2020-05-02 16:58:25 +0530

    Calories in home page

--- lib/pages/dashboard/home.dart
+++ lib/pages/dashboard/home.dart
@@ -10,8 +10,26 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            body: Center(child: Text('home')),
+            body: Center(child: getListView()),
 
     );
   }
+}
+
+ Widget getListView() {
+  var listView =ListView(
+    children: <Widget>[
+      Container(
+       padding:EdgeInsets.all(25) ,
+       child: ListTile(
+        leading: Icon(Icons.favorite ,size :20,color:Colors.red),
+        title: Text('Calories :'),
+        subtitle: Text("Calories Consumed for the Day")
+      ),
+       )
+    ]
+  );
+
+      return listView;
+      
 }
